10be563e3d17054181e1ad41a894787e43c417cd
mhaxen <m.haxen@gmail.com>
2022-12-20 13:01:12 +0100

    stylize

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,10 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/CV.vue
+++ src/components/CV.vue
@@ -3,81 +3,107 @@
    <div class="headline">
     <h1>Magnus Haxen</h1>
    </div>
-   <div class="intro section">
-    <h2>Hi there!</h2>
-    <p>
-      My name is Magnus Haxen, and I am a huge enthusiast of deep learning and artificial intelligence. I recently graduated from Aarhus University with a Bachelor's degree in Computer Science, where I focused on AI and deep learning as part of my studies.
-      During my time at Aarhus University, I had the opportunity to learn from some of the leading experts in the field and work on various projects that allowed me to apply my knowledge in a practical setting. I also had the unique experience of completing half of my degree during the pandemic, which presented its own set of challenges but ultimately helped me to become even more resilient and adaptable.
-      In my free time, you can find me staying up-to-date on the latest research and developments within the field of AI and deep learning. My ultimate career goal is to land a job at a well-respected company where I can put my skills and passion to good use and make a positive impact in the world of artificial intelligence. With my dedication, drive, and now a solid foundation in computer science and AI, I am confident that I can achieve this dream and more.
-    </p>
+   <div class="intro">
+    <div class="section">
+      <h2>Hi there!</h2>
+      <p>
+        My name is Magnus Haxen, and I am a huge enthusiast of deep learning and artificial intelligence. I recently graduated from Aarhus University with a Bachelor's degree in Computer Science, where I focused on AI and deep learning as part of my studies.
+        During my time at Aarhus University, I had the opportunity to learn from some of the leading experts in the field and work on various projects that allowed me to apply my knowledge in a practical setting. I also had the unique experience of completing half of my degree during the pandemic, which presented its own set of challenges but ultimately helped me to become even more resilient and adaptable.
+        In my free time, you can find me staying up-to-date on the latest research and developments within the field of AI and deep learning. My ultimate career goal is to land a job at a well-respected company where I can put my skills and passion to good use and make a positive impact in the world of artificial intelligence. With my dedication, drive, and now a solid foundation in computer science and AI, I am confident that I can achieve this dream and more.
+      </p>
+    </div>
    </div>
-   <div class="education section">
-    <h3>Education</h3>
-    <ul>
-      <li>
-        Bachelor's Degree in Computer Science, Institution, 2021
-      </li>
-    </ul>
-   </div>
-   <div class="publications section">
-    <h3>Publications</h3>
-    <ul>
-      <li>
-        <a href="https://dl.acm.org/doi/10.1145/3459637.3482072">
-          "Center Point Query Authentication", CIKM2021 conference, 2021
-        </a>
-      </li>
-    </ul>
-   </div>
-   <div class="work_experience section">
-    <h3>Work Experience</h3>
-    <ul>
-      <li>
-        Software Developer, Startup Company, 2021-2022
-      </li>
-    </ul>
-   </div>
-   <div class="online_courses section">
-    <h3>Online Courses</h3>
-    <ul>
-      <li>
-        Machine Learning, Institution, Year
-      </li>
-      <li>
-        Deep Learning, Institution, Year
-      </li>
-      <li>
-        AI, Institution, Year
-      </li>
-    </ul>
-   </div>
-   <div class="skills section">
-  <h3>Skills</h3>
-  <ul>
-    <li>Vue.js</li>
-    <li>C#</li>
-    <li>Go</li>
-    <li>Python</li>
-    <li>JavaScript</li>
-    <li>Familiar with the following Python frameworks for machine learning, deep learning, and AI:
+   <div class="education">
+    <div class="section">
+      <h2>Education</h2>
       <ul>
-        <li>NumPy</li>
-        <li>Pandas</li>
-        <li>Scikit-learn</li>
-        <li>TensorFlow</li>
-        <li>PyTorch</li>
-        <li>Keras</li>
+        <li>
+          Bachelor's Degree in Computer Science, Institution, 2021
+        </li>
       </ul>
-    </li>
-  </ul>
-</div>
-   <div class="contact section">
-    <h3>Contact</h3>
+    </div>
+   </div>
+   <div class="publications">
+    <div class="section">
+      <h2>Publications</h2>
+      <ul>
+        <li>
+          <a href="https://dl.acm.org/doi/10.1145/3459637.3482072">
+            "Center Point Query Authentication", CIKM2021 conference, 2021
+          </a>
+        </li>
+      </ul>
+    </div>
+   </div>
+   <div class="work_experience">
+    <div class="section">
+      <h2>Work Experience</h2>
+      <ul>
+        <li>
+          Software Developer, Startup Company, 2021-2022
+        </li>
+      </ul>
+    </div>
+   </div>
+   <div class="online_courses">
+    <div class="section">
+      <h2>Online Courses</h2>
+      <p>
+        Throughout my journey in the field of artificial intelligence, I have dedicated myself to continuously learning and expanding my skillset. This dedication led me to enroll in the "Machine Learning A-Z" course on Udemy, where I gained a strong foundation in machine learning techniques and algorithms.
+        Building on this foundation, I then enrolled in the "Deep Learning A-Z" course, where I delved into the exciting world of deep learning and gained a thorough understanding of neural networks and their applications.
+        Finally, I completed the "Artificial Intelligence A-Z" course, further enhancing my knowledge and skills in the field of AI. These courses not only provided me with a strong theoretical understanding, but also equipped me with the practical skills needed to apply these concepts to real-world problems.
+        Through my dedication to learning and my successful completion of these courses, I am confident in my ability to continue making meaningful contributions to the field of artificial intelligence.
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.udemy.com/course/machinelearning/">
+            Machine Learning A-Z, Udemy
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/course/deeplearning/">
+            Deep Learning A-Z, Udemy
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/course/artificial-intelligence-az/">
+            Artificial Intelligence A-Z, Udemy
+          </a>
+        </li>
+      </ul>
+    </div>
+   </div>
+   <div class="skills">
+   <div class="section">
+    <h2>Skills</h2>
     <ul>
-      <li>Email: magnushaxen97@gmail.com</li>
-      <li>Phone: +45 51 32 67 15</li>
-      <li>LinkedIn: <a href="https://www.linkedin.com/in/magnushaxen/">www.linkedin.com/in/magnushaxen/</a></li>
+      <li>Vue.js</li>
+      <li>C#</li>
+      <li>Go</li>
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>Familiar with the following Python frameworks for machine learning, deep learning, and AI:
+        <ul>
+          <li>NumPy</li>
+          <li>Pandas</li>
+          <li>Scikit-learn</li>
+          <li>TensorFlow</li>
+          <li>PyTorch</li>
+          <li>Keras</li>
+        </ul>
+      </li>
     </ul>
+   </div>
+</div>
+   <div class="contact">
+    <div class="section">
+      <h2>Contact</h2>
+      <ul>
+        <li>Email: magnushaxen97@gmail.com</li>
+        <li>Phone: +45 51 32 67 15</li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/magnushaxen/">www.linkedin.com/in/magnushaxen/</a></li>
+      </ul>
+    </div>
    </div>
   </div>
 </template>
@@ -104,6 +130,8 @@ body {
   max-width: 800px;
   margin: 0 auto;
   text-align: justify;
+  padding-top: 32px;
+  padding-bottom: 32px;
 }
 
 /* Add a nice blue color to links */
@@ -129,6 +157,14 @@ footer {
   background-color: #f2f2f2;
   color: #999;
   text-align: center;
+}
+
+.intro, .publications, .online_courses, .contact {
+  background-color: #e4f1fe;
+}
+
+.contact {
+  padding-bottom: 32px;
 }
 
 
